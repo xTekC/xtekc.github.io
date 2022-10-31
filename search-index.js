fc5b0bfb004743d806b5a677341409e3ec2d8064
xTeKc <xTeKc@users.noreply.github.com>
2022-10-31 04:12:00 +0000

    deploy: 8c0f4c58b41d170b461e29ac05ab1696bba7e46b

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"xTekC":{"doc":"Hello, I am xTekC.","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+"xTekC":{"doc":"Hello, I am xTekC. …","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
