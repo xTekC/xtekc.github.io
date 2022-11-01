88e7dd657c89095f70df43de559cf40b47f1e8dd
xTekC <bcd3v@pm.me>
2022-10-31 23:33:42 -0400

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,58 @@
+//! &nbsp; [Github](https://github.com/xTekC) &nbsp; [Twitter](https://twitter.com/xTekC) &nbsp; [Blog](https://xtekc.hashnode.dev)
+//!
 //! Hello, I am xTekC.
-//! ....
+//!
+//! Interests:
+//! - Linux
+//! - Rust
+//! - Privacy
+//! - Security
+
+/// Open Source Developer (2+ Years)
+pub mod open_source_exp {}
+
+/// Production Engineer (10+ Years)
+pub mod production_engineer_exp {}
+
+/// Rust
+#[macro_export]
+macro_rules! Rust {
+    () => {};
+}
+
+/// Bash
+#[macro_export]
+macro_rules! Bash {
+    () => {};
+}
+
+/// C
+#[macro_export]
+macro_rules! C {
+    () => {};
+}
+
+/// Certification Road-Map
+pub struct Certifications {
+   pub LinuxPlus: String,
+   pub ServerPlus: String,
+   pub CloudPlus: String,
+   pub NetworkPlus: String,
+   pub SecurityPlus: String,
+   pub CysaPlus: String,
+   pub PentestPlus: String,
+   pub CaspPlus: String,
+}
+
+/// Comptia Linux+ (pending)
+pub const CERT01: &str = "Linux+";
+
+/// A markdown to html compiler
+/// [github.com/xTekC/mduc](https://github.com/xTekC/mduc)
+pub fn mduc() {}
+
+/// Delete data from shell history file(s)
+/// [github.com/xTekC/shdel](https://github.com/xTekC/shdel)
+pub fn shdel() {}
+
+// Soft Skills
