4df9a8d0074444deec85fc6c8c4b6977fd3df0ed
xTeKc <xTeKc@users.noreply.github.com>
2022-11-01 03:34:08 +0000

    deploy: 88e7dd657c89095f70df43de559cf40b47f1e8dd

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xTekC"] = [{"text":"impl Freeze for <a class=\"struct\" href=\"xTekC/struct.Certifications.html\" title=\"struct xTekC::Certifications\">Certifications</a>","synthetic":true,"types":["xTekC::Certifications"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.Unpin.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xTekC"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"xTekC/struct.Certifications.html\" title=\"struct xTekC::Certifications\">Certifications</a>","synthetic":true,"types":["xTekC::Certifications"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
