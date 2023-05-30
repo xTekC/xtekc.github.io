2335d876fc745fa47735c99d61c8eda65f8606f9
xTeKc <xTeKc@users.noreply.github.com>
2023-05-30 07:16:17 +0000

    deploy: 591c2b274cd4ac3d7235c00bef72f925d2dda4fc

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"xTekC":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"xTekC/struct.Certifications.html\" title=\"struct xTekC::Certifications\">Certifications</a>",1,["xTekC::Certifications"]]]
+"xTekC":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"xTekC/struct.Certifications.html\" title=\"struct xTekC::Certifications\">Certifications</a>",1,["xTekC::Certifications"]]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
